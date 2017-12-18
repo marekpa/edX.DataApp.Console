c296f7ed922a844b62c56d80fd91e3e98f2736b2
paralicm <marek.paralic@winpos.com>
2017-12-18 13:44:48 +0100

    Add project files.

---
+++ edX.DataApp.Console/GenericQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace edX.DataApp.Console
+{
+    public class GenericQuery
+    {
+        public async Task<IEnumerable<Customer>> RunLogic(SqlConnection connection)
+        {
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT TOP(25) CustomerID, CompanyName, EmailAddress, Phone FROM Customers";
+            List<Customer> customerList = new List<Customer>();
+            SqlDataReader reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                Customer item = new Customer()
+                {
+                    Id = reader.GetInt32(0),
+                    Company = reader.GetString(1),
+                    Email = reader.GetString(2),
+                    Phone = reader.GetString(3)
+                };
+                customerList.Add(item);
+            }
+            return customerList;
+        }
+
+    }
+}
---
+++ edX.DataApp.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace edX.DataApp.Console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            RunAsync().Wait();
+            System.Console.WriteLine("Application has completed execution. Press any key to exit.");
+            System.Console.ReadKey();
+        }
+
+        public static async Task RunAsync()
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLOCALDB;Initial Catalog=ContosoDB;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                System.Console.WriteLine("Connection Successful");
+                //await new DataAdapter().RunLogic(connection);
+                //await new BasicQuery().RunLogic(connection);
+                //IEnumerable<Customer> customers = await new GenericQuery().RunLogic(connection);
+                //foreach (Customer customer in customers)
+                //{
+                //    System.Console.WriteLine($"[{customer.Id:000}]\t{customer.Company}\t{customer.Email}");
+                //}
+            }
+        }
+    }
+}
